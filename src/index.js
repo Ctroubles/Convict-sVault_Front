3447779c8e0d9ccb07bcd8ba05ad7f1092b5cea5
kan4crooo <kevinacr17@gmail.com>
2023-06-05 16:54:19 -0500

    landing and login

--- src/index.js
+++ src/index.js
@@ -2,14 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom';
-import "./index.css"
-
-
+// import reportWebVitals from "./reportWebVitals";
+import "./index.css";
+import { Auth0Provider } from '@auth0/auth0-react';
+const domain= process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId= process.env.REACT_APP_AUTH0_CLIENT_ID;
 ReactDOM.render(
   <React.StrictMode>
+    <Auth0Provider 
+    domain={domain} 
+    clientId={clientId}
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/views/Landing/Landing.jsx
+++ src/views/Landing/Landing.jsx
@@ -1,10 +1,15 @@
 import style from "./Landing.module.css";
-
+import {Link} from "react-router-dom"
+import { useAuth0 } from "@auth0/auth0-react";
 const Landing = () =>{
+    const { loginWithRedirect } = useAuth0();
     return(
-        <h1>
-            Landing
-        </h1>
+        <div className={style.container}>
+            <Link to={"/home"} >
+            <button className={style.button}>Ir a la tienda</button>
+            </Link>
+            <button className={style.button} onClick={()=>loginWithRedirect()}>Ingresar</button>
+        </div>
     )
 };
 
