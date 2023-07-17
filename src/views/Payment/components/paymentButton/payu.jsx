ec4f51b4e547356fa4984997e68e6258c3908e60
kan4crooo <kevinacr17@gmail.com>
2023-07-17 14:57:19 -0500

    actualizacion de la descripcion en el form

--- src/views/Payment/components/paymentButton/payu.jsx
+++ src/views/Payment/components/paymentButton/payu.jsx
@@ -1,77 +1,25 @@
-import React, { useState } from 'react';
+import React from 'react';
 import style from "./paymentButton.module.css"
 import { v4 as uuidv4 } from 'uuid';
 import { validatorsLevel2 } from '../../validators';
-import axios from 'axios';
-import Url_deploy_back from '../../../../util/deploy_back';
 
 const CryptoJS = require("crypto-js");
 
+
 function PAYU({total, user, formRef, setErrors, items}) {
-  console.log(items)
+console.log(items)
   let apiKey = "4Vj8eK4rloUd272L48hsrarnUA";
   let merchantId = "508029";
   const referenceCode = `PAGO${uuidv4()}`;
   let mont = total;
 
   let signature = CryptoJS.MD5(apiKey + "~" + merchantId + "~" + referenceCode + "~" + mont + "~COP").toString();
-  
-  const [paymentId, setPaymentId] = useState('');
-
-  const handleCapturePayment = async (paymentId) => {
-    try {
-      // Realiza una solicitud POST a la API de PayU para capturar el pago
-      const response = await axios.post(
-        `https://api.payulatam.com/payments-api/${paymentId}/capture`,
-        {
-          // Aquí debes proporcionar los parámetros necesarios para capturar el pago, como la clave de la API y otros datos específicos
-        }
-      );
-  
-      // El pago se capturó exitosamente
-      console.log('Pago capturado:', response.data);
-  
-      // Envía los datos del pago capturado a la base de datos
-      const transactionData = {
-        paymentId: paymentId,
-        amount: response.data.amount,
-        // Otros datos relevantes del pago que desees enviar a la base de datos
-      };
-  
-      const databaseResponse = await axios.post(`${Url_deploy_back}/transactions/create`, transactionData);
-      console.log('Pago enviado a la base de datos:', databaseResponse.data);
-    } catch (error) {
-      // Ocurrió un error al capturar el pago
-      if (error.response && error.response.data) {
-        console.error('Error al capturar el pago:', error.response.data);
-      } else {
-        console.error('Error al capturar el pago:', error.message);
-      }
-    }
-  };
-
-  const createPayment = async () => {
-    try {
-      const response = await axios.post('https://api.payu.com/v2_1/payments', {
-        // ... otros parámetros necesarios para crear el pago
-      });
-      const paymentId = response.data.id;
-      setPaymentId(paymentId);
-    } catch (error) {
-      console.error('Error al crear el pago:', error);
-    }
-  };
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-
-    if (!validatorsLevel2(setErrors, formRef.current)) {
+    
+  const handleSubmit = (event) => {
+    if (!validatorsLevel2(setErrors,formRef.current)) {
+      event.preventDefault(); 
       return;
     }
-    
-    await createPayment();
-    console.log("hola", paymentId);
-    await handleCapturePayment(paymentId);
   };
 
   return (
@@ -79,7 +27,16 @@ function PAYU({total, user, formRef, setErrors, items}) {
       <form method="post" action="https://sandbox.checkout.payulatam.com/ppp-web-gateway-payu/" onSubmit={handleSubmit}>
         <input name="merchantId" type="hidden" value="508029" />
         <input name="accountId" type="hidden" value="512321" />
-        <input name="description" type="hidden" value={`${items[0].name} x ${items[0].quantity}`} />
+        <input
+        name="description"
+        type="hidden"
+        value={items
+          .map((item) => {
+            const { _id, name, quantity } = item;
+            return `ID: ${_id} - Nombre: ${name} (Cantidad: ${quantity})`;
+          })
+          .join(", ")}
+      />
         <input name="referenceCode" type="hidden" value={referenceCode} />
         <input name="amount" type="hidden" value={total} />
         <input name="tax" type="hidden" value="0" />
