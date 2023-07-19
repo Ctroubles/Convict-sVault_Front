83bc6120ed185d3816e7e9404536cca7bad4c11a
kan4crooo <kevinacr17@gmail.com>
2023-07-19 14:25:59 -0500

    confirmationPage V0.7

--- src/views/Payment/components/paymentButton/PayuConfirmationPage.jsx
+++ src/views/Payment/components/paymentButton/PayuConfirmationPage.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect } from 'react';
 import { useLocation, useHistory } from 'react-router-dom';
+import Url_deploy_back from '../../../../util/deploy_back';
+import axios from 'axios';
 
 function PayUConfirmationPage() {
   const location = useLocation();
@@ -9,14 +11,19 @@ function PayUConfirmationPage() {
     const searchParams = new URLSearchParams(location.search);
     const transactionState = searchParams.get('transactionState');
     const responseCode = searchParams.get('responseCode');
+    const transactionId = searchParams.get('transactionId');
+    const extra1 = searchParams.get('extra1');
+    const TX_VALUE = searchParams.get('TX_VALUE');
 
     console.log('transactionState:', transactionState);
     console.log('responseCode:', responseCode);
 
     // Realiza las acciones necesarias según el estado de la transacción y el código de respuesta
-    if (transactionState === 'APPROVED') {
+    if (transactionState === '4' && responseCode === 'APPROVED') {
       // Transacción confirmada exitosamente
       console.log('Transacción confirmada exitosamente');
+      saveTransaction(transactionId, extra1, TX_VALUE); // Guardar transacción en la base de datos
+      updateStockFromDescription(searchParams, transactionId); // Actualizar el stock de los productos
       history.push('/success'); // Redirige a la página de éxito
     } else {
       // Transacción fallida u otro caso
@@ -24,6 +31,82 @@ function PayUConfirmationPage() {
       history.push('/error'); // Redirige a la página de error
     }
   }, [location.search, history]);
+
+  const saveTransaction = async (transactionId, extra1, TX_VALUE) => {
+    try {
+      const response = await fetch(`${Url_deploy_back}/transactions/create`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          transactionId,
+          total: TX_VALUE,
+          description: extra1,
+          state: 'APPROVED', // Puedes ajustar el estado según tus necesidades
+        }),
+      });
+      if (response.ok) {
+        console.log('Transacción guardada en la base de datos');
+      } else {
+        console.error('Error al guardar la transacción en la base de datos');
+      }
+    } catch (error) {
+      console.error('Error al realizar la solicitud POST:', error.message);
+    }
+  };
+
+  const updateStockFromDescription = (searchParams, transactionId) => {
+    const description = searchParams.get('description');
+    const productInfoArray = description.split(', ');
+
+    productInfoArray.forEach(async (productInfo) => {
+      const [productId, quantityString] = productInfo
+        .replace('ID: ', '')
+        .split(' - Nombre: ');
+
+      const quantity = parseInt(/\(Cantidad: (\d+)\)/.exec(quantityString)[1], 10);
+
+      if (!isNaN(quantity)) {
+        // Verificar si el ID de transacción ya ha sido procesado
+        if (localStorage.getItem(transactionId)) {
+          console.log(`La actualización de stock para la transacción ${transactionId} ya ha sido procesada`);
+          return;
+        }
+
+        await updateProductStock(productId, quantity, transactionId);
+
+        // Almacenar el ID de transacción en el almacenamiento local para marcarlo como procesado
+        localStorage.setItem(transactionId, 'processed');
+      }
+    });
+  };
+
+  const updateProductStock = async (productId, quantity, transactionId) => {
+    try {
+      // Obtener datos del producto
+      const { data } = await axios.get(`${Url_deploy_back}/products/${productId}`);
+      const { name, price, image, brand, category, stock } = data;
+
+      // Actualizar el stock
+      let updatedStock = stock - quantity;
+      updatedStock = Math.max(updatedStock, 0);
+      console.log(updatedStock)
+
+      // Realizar la solicitud PUT para actualizar el stock del producto
+      await axios.put(`${Url_deploy_back}/products/${productId}`, {
+        name,
+        price,
+        image,
+        brand,
+        category,
+        stock: updatedStock,
+      });
+
+    } catch (error) {
+      console.error('Error al actualizar el stock del producto:', error.message);
+    }
+  };
 
   return (
     <div>
