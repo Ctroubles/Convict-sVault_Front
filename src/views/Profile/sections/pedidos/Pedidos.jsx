c1c8e9a7f3f38b4485b39a9aba541eb6090143ca
kan4crooo <kevinacr17@gmail.com>
2023-11-01 04:26:54 -0500

    cambios en pedidos.jsx para obtener las ordenes de una persona ahora en el perfil se ven los pedidos del usuario

--- src/views/Profile/sections/pedidos/Pedidos.jsx
+++ src/views/Profile/sections/pedidos/Pedidos.jsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import style from './Pedidos.module.css';
+import axios from 'axios';
 
-const Pedidos = ({ orders=[] }) => {
-    console.log(orders)
+const Pedidos = ({ pedidos = [] }) => {
+  const [productDetails, setProductDetails] = useState([]);
 
-  if (orders.length === 0) {
+  useEffect(() => {
+    // Función para obtener detalles de productos por ID
+    const fetchProductDetails = async (productId) => {
+      try {
+        const response = await axios.get(`http://localhost:3001/products/${productId}`);
+        const productData = response.data;
+
+        // Añadir los detalles del producto al estado
+        setProductDetails((prevProductDetails) => [...prevProductDetails, productData]);
+      } catch (error) {
+        console.error('Error al obtener detalles del producto:', error);
+      }
+    };
+
+    // Resetear los detalles de productos cada vez que cambia el array de pedidos
+    setProductDetails([]);
+
+    // Iterar a través de los pedidos y obtener los detalles de cada producto
+    pedidos.forEach((pedido) => {
+      fetchProductDetails(pedido);
+    });
+  }, [pedidos]);
+
+  if (pedidos.length === 0) {
     return (
       <div id={style.Direcciones}>
         <div id={style.noData}>
@@ -18,21 +42,29 @@ const Pedidos = ({ orders=[] }) => {
     <div id={style.Direcciones}>
       <h2>Tus Pedidos</h2>
       <div>
-        {orders.map((order, index) => (
-          <div key={index} className={style.pedidoItem}>
-            <h3>Pedido {index + 1}</h3>
-            <ul>
-              {order.products.map((product, productIndex) => (
-                <li key={productIndex}>
-                  {product.name} - Cantidad: {product.quantity}
-                </li>
-              ))}
-            </ul>
-          </div>
-        ))}
+        {productDetails.length > 0 &&
+          productDetails.map((product, index) => (
+            <div key={index} className={style.pedidoItem}>
+              <h3> {index + 1}</h3>
+              <div className={style.productInfo}>
+                <div className={style.imageContainer}>
+                  <img src={product.image} alt={product.name} />
+                </div>
+                <div className={style.detailsContainer}>
+                  <ul>
+                    <li>Nombre: {product.name}</li>
+                    <li>Precio: {product.price}</li>
+                    <li>Marca: {product.brand}</li>
+                    <li>
+                      Categoría: {product.category && product.category.length > 0 ? product.category.join(', ') : 'N/A'}
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          ))}
       </div>
     </div>
   );
 };
-
 export default Pedidos;
